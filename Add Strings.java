0ed487ab40eca54905a9525e1d4bc7b379bdf5da
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2021-04-07 03:04:55 +0530

    Create Add Strings.java

---
+++ Add Strings.java
@@ -0,0 +1,13 @@
+
+import java.math.BigInteger;
+class Solution {
+    public String addStrings(String num1, String num2)
+    {
+       BigInteger l1=new BigInteger(num1);
+        
+        BigInteger l2=new BigInteger(num2);
+    
+         BigInteger sum=l1.add(l2);
+        return ""+sum;
+    }
+}
