ce6393917073b7749f4910f6c570990d17b235f2
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2021-06-28 23:06:11 +0530

    Add files via upload

---
+++ Rotate.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+   public class Rotate{
+
+   public static void main(String[] args)
+    {
+        int a=244513;
+        int b=10;
+          String s=""+a;
+       int len=s.length();
+       System.out.println(len);
+       int k=-10;
+       k=k%len;
+       if(k<0)
+       {
+		k=len+k;
+	   }
+	   System.out.println("k="+k);
+
+	   int mult=(int)Math.pow(10,len-k);
+	   int div=(int)Math.pow(10,k);
+
+	   System.out.println(mult);
+
+	   System.out.println(div);
+	   int lastkdig=a%div;
+	   int firstkdig=a/div;
+
+	   System.out.println(lastkdig);
+
+	   System.out.println("f="+firstkdig);
+
+    }
+   }
