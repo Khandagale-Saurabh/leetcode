fa7f2e915e9226cb13f736ab34295bacb5c485b5
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2021-09-09 18:56:06 +0530

    Update Add Strings.java

--- Add Strings.java
+++ Add Strings.java
@@ -6,7 +6,6 @@ class Solution {
        BigInteger l1=new BigInteger(num1);
         
         BigInteger l2=new BigInteger(num2);
-    
          BigInteger sum=l1.add(l2);
         return ""+sum;
     }
